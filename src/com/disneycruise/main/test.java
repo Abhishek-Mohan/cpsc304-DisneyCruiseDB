34f79f5a02a41f6820f2811a4ca76dce05e71705
Abhishek Mohan <Abhishek Mohan>
2017-03-13 18:42:25 -0700

    Added JDBC Connection, still testing it out

---
+++ src/com/disneycruise/main/test.java
@@ -0,0 +1,26 @@
+package com.disneycruise.main;
+
+import java.sql.DriverManager;
+import java.sql.*;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by abhis on 3/8/2017.
+ */
+public class test
+{
+    public static void main (String args []) throws SQLException, IOException
+    {
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        Connection conn = DriverManager.getConnection(
+                "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug", "ora_i2c0b", "a41374142");
+
+
+    }
+
+
+
+}
--- src/test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by abhis on 3/8/2017.
- */
-public class test {
-}
